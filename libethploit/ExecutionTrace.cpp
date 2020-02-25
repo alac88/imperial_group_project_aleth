2d5bf806ca29e4f78b3a4ae1f2aeb2b805983f05
Ziyun Poh <zp619@corona19.doc.ic.ac.uk>
2020-02-25 09:01:40 +0000

    add branch coverage

--- libethploit/ExecutionTrace.cpp
+++ libethploit/ExecutionTrace.cpp
@@ -2,44 +2,43 @@
 
 namespace dev 
 {
-namespace eth 
-{
+    namespace eth 
+    {
 
-ExecutionTrace::ExecutionTrace(Instruction _instruction, 
-                                Address _senderAddress,
-                                Address _receiveAddress,
-                                u256 _gas,
-                                uint64_t _m_PC, 
-                                u256* _m_SP,
-                                u256* _m_SPP)
-{
-    instruction = _instruction;
-    senderAddress = _senderAddress;
-    receiveAddress = _receiveAddress;
-    gas = _gas;
-    m_PC = _m_PC;
-    m_SP = _m_SP;
-    m_SPP = _m_SPP;
-}
+        ExecutionTrace::ExecutionTrace(Instruction _instruction, 
+                                        Address _senderAddress,
+                                        Address _receiveAddress,
+                                        u256 _gas,
+                                        uint64_t _m_PC, 
+                                        u256* _m_SP,
+                                        u256* _m_SPP)
+        {
+            instruction = _instruction;
+            senderAddress = _senderAddress;
+            receiveAddress = _receiveAddress;
+            gas = _gas;
+            m_PC = _m_PC;
+            m_SP = _m_SP;
+            m_SPP = _m_SPP;
+        }
 
-void ExecutionTrace::setReturningPC(uint64_t _m_PC) 
-{
-    m_PC_post = _m_PC;
-}
+        void ExecutionTrace::setReturningPC(uint64_t _m_PC) 
+        {
+            m_PC_post = _m_PC;
+        }
 
-void ExecutionTrace::print() {
+        void ExecutionTrace::print() {
 
-    InstructionInfo info = instructionInfo(instruction);
-    std::cout << info.name << std::endl;
-    // std::cout << instruction << std::endl;
-    // std::cout << senderAddress << std::endl;
-    // std::cout << receiveAddress << std::endl;
-    // std::cout << gas << std::endl;
-    // std::cout << m_PC << std::endl;
-    // std::cout << m_SP << std::endl;
-    // std::cout << m_SPP << std::endl;
+            InstructionInfo info = instructionInfo(instruction);
+            std::cout << info.name << std::endl;
+            // std::cout << instruction << std::endl;
+            // std::cout << senderAddress << std::endl;
+            // std::cout << receiveAddress << std::endl;
+            // std::cout << gas << std::endl;
+            // std::cout << m_PC << std::endl;
+            // std::cout << m_SP << std::endl;
+            // std::cout << m_SPP << std::endl;
 
-}
-
-}
-}
+        }
+    }
+} // LCOV_EXCL_LINE
