541354343e3000e236060e59b1261cf40914019e
Ziyun Poh <zp619@vm-shell2.doc.ic.ac.uk>
2020-04-15 00:34:41 +0100

    Remove print test

--- libethploittest/executiontrace-test/ExecutionTraceTest.cpp
+++ libethploittest/executiontrace-test/ExecutionTraceTest.cpp
@@ -10,7 +10,7 @@ BOOST_AUTO_TEST_SUITE(ExecutionTraceTestSuite)
   Instruction instruction = Instruction::CALL;
   Address sender ("1122334455667788991011121314151617181920");
   Address receiver ("1122334455667788991011121314151617181921");
-  u256 valueTransfer = 0;
+  u256 valueTransfer = 10;
 
   BOOST_AUTO_TEST_CASE(constructor_test)
   {
@@ -18,10 +18,30 @@ BOOST_AUTO_TEST_SUITE(ExecutionTraceTestSuite)
 
     BOOST_TEST_MESSAGE("Constructor test: ");
     // Check if the values are stored in ExecutionTrace attributes 
-    BOOST_TEST(execTrace.instruction == "CALL"); // TODO: perform better check here
+    BOOST_TEST(execTrace.instruction == "CALL"); 
     BOOST_TEST(execTrace.senderAddress == sender.hex());
     BOOST_TEST(execTrace.receiveAddress == receiver.hex());
     BOOST_TEST(execTrace.valueTransfer == valueTransfer);
+  }
+
+  BOOST_AUTO_TEST_CASE(unknown_instruction_gives_error_message)
+  {
+    ExecutionTrace execTrace (instruction, sender, receiver, valueTransfer);
+
+    ostringstream buffer;
+
+    // Save cout's buffer here
+    auto cout_buff = cout.rdbuf();
+
+    // Redirect cout to buffer
+    cout.rdbuf(buffer.rdbuf());
+    
+    execTrace.print();
+    
+    // When done redirect cout to its old self
+    cout.rdbuf(cout_buff);
+
+    BOOST_TEST(string("CALL\n") == buffer.str(), boost::test_tools::per_element());
   }
 
 BOOST_AUTO_TEST_SUITE_END()
